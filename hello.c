730ecddd0853af792b57f4f8513d3eb946a4aadf
Kamran Rasheed <58663501+Kamran-360@users.noreply.github.com>
2020-09-30 22:32:24 -0700

    readability improved

--- hello.c
+++ hello.c
@@ -3,9 +3,10 @@ void main()
 {
 printf("WELCOME TO HORIZON 2K19");
   
-printf("C program"); 
+printf("C program");
+//   "hello world!"
+//   printf("This is a small change in Program");
+  
   getch();
 }
-"hello world!"
-  printf("This is a small change in Program");
-  
+
